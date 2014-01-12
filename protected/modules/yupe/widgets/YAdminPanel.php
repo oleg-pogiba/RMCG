57539c4f08fc0a5852e1aa839dd3725013a66c8d
oleg-pogiba <workposst@gmail.com>
2014-01-12 19:36:49 +0200

    временно откл, так как куширует панель переключения языков

--- protected/modules/yupe/widgets/YAdminPanel.php
+++ protected/modules/yupe/widgets/YAdminPanel.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Виджет админ-панели для фронтальной части сайта
  *
@@ -8,61 +9,63 @@
  **/
 class YAdminPanel extends YWidget
 {
-    public $view = 'application.modules.yupe.views.widgets.YAdminPanel.adminpanel';
+	public $view = 'application.modules.yupe.views.widgets.YAdminPanel.adminpanel';
 
-    public function init()
-    {
-        if (!isset($this->controller->yupe->bootstrap)) {
-            $this->controller->yupe->getComponent('bootstrap');
-        }
+	public function init()
+	{
+		if (!isset($this->controller->yupe->bootstrap)) {
+			$this->controller->yupe->getComponent('bootstrap');
+		}
 
-        parent::init();
-    }
+		parent::init();
+	}
 
-    /**
-     * Запуск виджета
-     *
-     * @return void
-     **/
-    public function run()
-    {
-        if (Yii::app()->user->isSuperUser()) {
-            $mainAssets = Yii::app()->assetManager->publish(Yii::getPathOfAlias('application.modules.yupe.views.assets'));
-            
-            Yii::app()->clientScript->registerCssFile($mainAssets . '/css/frontpanel.css');
-            Yii::app()->clientScript->registerScriptFile($mainAssets . '/js/main.js');
+	/**
+	 * Запуск виджета
+	 *
+	 * @return void
+	 **/
+	public function run()
+	{
+		if (Yii::app()->user->isSuperUser()) {
+			$mainAssets = Yii::app()->assetManager->publish(Yii::getPathOfAlias('application.modules.yupe.views.assets'));
 
-            Yii::app()->clientScript->registerScript(
-                'yupeToken', 'var actionToken = ' . json_encode(
-                    array(
-                        'token'      => Yii::app()->getRequest()->csrfTokenName . '=' . Yii::app()->getRequest()->csrfToken,
-                        'url'        => Yii::app()->createAbsoluteUrl('yupe/backend/modulestatus'),
-                        'message'    => Yii::t('YupeModule.yupe', 'Wait please, your request in process...'),
-                        'error'      => Yii::t('YupeModule.yupe', 'During the processing of your request an unknown error occurred =('),
-                        'loadingimg' => CHtml::image(
-                            $mainAssets . '/img/progressbar.gif', '', array(
-                                'style' => 'width: 100%; height: 20px;',
-                            )
-                        ),
-                        'buttons'    => array(
-                            'yes'    => Yii::t('YupeModule.yupe', 'Ok'),
-                            'no'     => Yii::t('YupeModule.yupe', 'Cancel'),
-                        ),
-                        'messages'   => array(
-                            'confirm_update'           => Yii::t('YupeModule.yupe', 'Do you really want to update configuration file?'),
-                            'confirm_deactivate'       => Yii::t('YupeModule.yupe', 'Do you really want to disable module?'),
-                            'confirm_activate'         => Yii::t('YupeModule.yupe', 'Do you really want to enable module?'),
-                            'confirm_uninstall'        => Yii::t('YupeModule.yupe', 'Do you really want to delete module?') . '<br />' . Yii::t('YupeModule.yupe', 'All module parameters will be deleted'),
-                            'confirm_install'          => Yii::t('YupeModule.yupe', 'Do you really want to install module?') . '<br />' . Yii::t('YupeModule.yupe', 'New module parameters will be added'),
-                            'confirm_cacheFlush'       => Yii::t('YupeModule.yupe', 'Do you really want to clean cache?'),
-                            'confirm_assetsFlush'      => Yii::t('YupeModule.yupe', 'Do you really want to clean assets?'),
-                            'confirm_cacheAssetsFlush' => Yii::t('YupeModule.yupe', 'Do you really want to clean cache and assets?') . '<br />' . Yii::t('YupeModule.yupe', 'This process can take much time!'),
-                            'unknown'                  => Yii::t('YupeModule.yupe', 'Unknown action was selected!'),
-                        )
-                    )
-                ), CClientScript::POS_BEGIN
-            );
+			Yii::app()->clientScript->registerCssFile($mainAssets . '/css/frontpanel.css');
+			Yii::app()->clientScript->registerScriptFile($mainAssets . '/js/main.js');
 
+			Yii::app()->clientScript->registerScript(
+				'yupeToken', 'var actionToken = ' . json_encode(
+					array(
+						'token' => Yii::app()->getRequest()->csrfTokenName . '=' . Yii::app()->getRequest()->csrfToken,
+						'url' => Yii::app()->createAbsoluteUrl('yupe/backend/modulestatus'),
+						'message' => Yii::t('YupeModule.yupe', 'Wait please, your request in process...'),
+						'error' => Yii::t('YupeModule.yupe', 'During the processing of your request an unknown error occurred =('),
+						'loadingimg' => CHtml::image(
+								$mainAssets . '/img/progressbar.gif', '', array(
+									'style' => 'width: 100%; height: 20px;',
+								)
+							),
+						'buttons' => array(
+							'yes' => Yii::t('YupeModule.yupe', 'Ok'),
+							'no' => Yii::t('YupeModule.yupe', 'Cancel'),
+						),
+						'messages' => array(
+							'confirm_update' => Yii::t('YupeModule.yupe', 'Do you really want to update configuration file?'),
+							'confirm_deactivate' => Yii::t('YupeModule.yupe', 'Do you really want to disable module?'),
+							'confirm_activate' => Yii::t('YupeModule.yupe', 'Do you really want to enable module?'),
+							'confirm_uninstall' => Yii::t('YupeModule.yupe', 'Do you really want to delete module?') . '<br />' . Yii::t('YupeModule.yupe', 'All module parameters will be deleted'),
+							'confirm_install' => Yii::t('YupeModule.yupe', 'Do you really want to install module?') . '<br />' . Yii::t('YupeModule.yupe', 'New module parameters will be added'),
+							'confirm_cacheFlush' => Yii::t('YupeModule.yupe', 'Do you really want to clean cache?'),
+							'confirm_assetsFlush' => Yii::t('YupeModule.yupe', 'Do you really want to clean assets?'),
+							'confirm_cacheAssetsFlush' => Yii::t('YupeModule.yupe', 'Do you really want to clean cache and assets?') . '<br />' . Yii::t('YupeModule.yupe', 'This process can take much time!'),
+							'unknown' => Yii::t('YupeModule.yupe', 'Unknown action was selected!'),
+						)
+					)
+				), CClientScript::POS_BEGIN
+			);
+
+			// временно откл, так как куширует панель переключения языков
+			/*
             $cached = Yii::app()->cache->get(
                 'YAdminPanel::' . Yii::app()->user->getId() . (
                     Yii::app()->controller instanceof yupe\components\controllers\BackController
@@ -70,19 +73,21 @@ class YAdminPanel extends YWidget
                     : 'frontend'
                 ) . '::' . Yii::app()->language
             );
+			*/
+			$cached = false;
 
-            if ($cached === false) {
-                $cached = $this->render($this->view, array(), true);
-                Yii::app()->cache->set(
-                    'YAdminPanel::' . Yii::app()->user->getId() . (
-                        Yii::app()->controller instanceof yupe\components\controllers\BackController
-                        ? 'backend'
-                        : 'frontend'
-                    ) . '::' . Yii::app()->language, $cached, 0, new TagsCache('yupe', 'YAdminPanel', 'installedModules')
-                );
-            }
+			if ($cached === false) {
+				$cached = $this->render($this->view, array(), true);
+				Yii::app()->cache->set(
+					'YAdminPanel::' . Yii::app()->user->getId() . (
+					Yii::app()->controller instanceof yupe\components\controllers\BackController
+						? 'backend'
+						: 'frontend'
+					) . '::' . Yii::app()->language, $cached, 0, new TagsCache('yupe', 'YAdminPanel', 'installedModules')
+				);
+			}
 
-            echo $cached;
-        }
-    }
+			echo $cached;
+		}
+	}
 }
